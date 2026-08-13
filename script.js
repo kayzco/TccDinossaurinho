9738ff8672fe02e82078ebfb0720663c01516553
kayzco <kaylaneacoliveira2009@gmail.com>
2026-08-12 23:46:27 -0300

    o up down

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 let bolinha = document.getElementById("bolinha");
-let posicao = 0;
+
+let posicaoX = 0;
+let posicaoY = (window.innerHeight / 2) - (bolinha.offsetHeight / 2);
+
+bolinha.style.left = posicaoX + "px";
+bolinha.style.top = posicaoY + "px";
 
 document.addEventListener("keydown", function(event) {
 
     if (event.key == "ArrowRight") {
-        posicao = posicao + 10;
+        posicaoX = posicaoX + 10;
     }
 
     if (event.key == "ArrowLeft") {
-        posicao = posicao - 10;
+        posicaoX = posicaoX - 10;
     }
 
-    bolinha.style.left = posicao + "px";
+    if (event.key == "ArrowUp") {
+        posicaoY = posicaoY - 10;
+    }
+
+    if (event.key == "ArrowDown") {
+        posicaoY = posicaoY + 10;
+    }
+
+    if (posicaoY > (window.innerHeight / 2) - (bolinha.offsetHeight / 2)) {
+        posicaoY = (window.innerHeight / 2) - (bolinha.offsetHeight / 2);
+    }
+
+    bolinha.style.left = posicaoX + "px";
+    bolinha.style.top = posicaoY + "px";
 
 });
