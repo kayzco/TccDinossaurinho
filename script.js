48d73944c68a7fdec62b89aa3f42e09b370332dd
lucca <luccalf13@gmail.com>
2026-08-26 18:43:18 -0300

    bolinha pulando
    bolinha agora pula, inseri a função de atualizar posição tambem

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 let bolinha = document.getElementById("bolinha");
-
 let posicaoX = 0;
 let posicaoY = (window.innerHeight / 2) - (bolinha.offsetHeight / 2);
-
+let velocidadeY = 0;
+let gravidade = 0.8;
+let forcaPulo = -15;
+let noChao = true;
 bolinha.style.left = posicaoX + "px";
 bolinha.style.top = posicaoY + "px";
 
+atualizarPosicao();
 document.addEventListener("keydown", function(event) {
 
-    if (event.key == "ArrowRight") {
-        posicaoX = posicaoX + 10;
-    }
-
-    if (event.key == "ArrowLeft") {
-        posicaoX = posicaoX - 10;
-    }
-
-    if (event.key == "ArrowUp") {
-        posicaoY = posicaoY - 10;
-    }
-
-    if (event.key == "ArrowDown") {
-        posicaoY = posicaoY + 10;
-    }
-
-    if (posicaoY > (window.innerHeight / 2) - (bolinha.offsetHeight / 2)) {
-        posicaoY = (window.innerHeight / 2) - (bolinha.offsetHeight / 2);
+    if (event.key == "ArrowRight") posicaoX += 10;
+    if (event.key == "ArrowLeft") posicaoX -= 10;
+    if (event.key == "ArrowUp" && noChao) {
+        velocidadeY = forcaPulo;
+        noChao = false
     }
 
     bolinha.style.left = posicaoX + "px";
-    bolinha.style.top = posicaoY + "px";
 
 });
+
+function pular() {
+    velocidadeY += gravidade;
+    posicaoY += velocidadeY;
+
+    let chao = (window.innerHeight / 2) - (bolinha.offsetHeight / 2);
+    if (posicaoY >= chao) {
+        posicaoY = chao;
+        velocidadeY = 0;
+        noChao = true;
+    }
+
+    bolinha.style.top = posicaoY + "px";
+    requestAnimationFrame(pular);
+}
+
+function atualizarPosicao() {
+    bolinha.style.left = posicaoX + "px";
+    bolinha.style.top = posicaoY + "px";
+}
+
+pular();
